1035297622b57dd0b5dcf6646680a4114f7c9588
Nosabua2000 <Nosabua123>
2020-02-26 09:35:56 +0100

    added new check methods

--- src/Poker.java
+++ src/Poker.java
@@ -3,6 +3,7 @@ public class Poker {
     public static final int CardsPerColor = 13;
     public static final int handCards = 5;
     public static final int[] CardDeck = new int[totalCards];
+    public static boolean royalflash = false;
     //Ass, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Dame, König -> Ass, 2...
     // 0,  1, 2, 3, 4, 5, 6, 7, 8, 9,  10,   11,   12,      13, 14...
     //0%13 = 0 -> Ass   13%13 = 0 -> Ass
@@ -13,31 +14,30 @@ public class Poker {
         }
     }
 
-    public static int[] deal(int[] cardarray,int number){
-        int[]  random = new int[number];
-        for(int i=0;i<number;i++){
-            int rand = (int)(Math.random()*(cardarray.length-1-i));
+    public static int[] deal(int[] cardarray, int number) {
+        int[] random = new int[number];
+        for (int i = 0; i < number; i++) {
+            int rand = (int) (Math.random() * (cardarray.length - 1 - i));
             random[i] = cardarray[rand];
             int storage = cardarray[rand];
-            cardarray[rand] = cardarray[cardarray.length-1-i];
-            cardarray[cardarray.length-1-i] = storage;
+            cardarray[rand] = cardarray[cardarray.length - 1 - i];
+            cardarray[cardarray.length - 1 - i] = storage;
         }
         return random;
     }
 
 
-    public static int CardColor(int card) {
+    public static int cardColor(int card) {
         return (card % CardsPerColor);
     }
 
 
-
-    public static int CardValue(int card) {
+    public static int cardValue(int card) {
         return (card / CardsPerColor);
     }
 
 
-    public static int DupeValues(int[] cardarray) {
+    public static int dupeValues(int[] cardarray) {
         int duplicateValues = 0;
         for (int i = 0; i < cardarray.length; i++) {
             for (int j = 0; j < cardarray.length; j++) {
@@ -49,7 +49,7 @@ public class Poker {
         return duplicateValues;
     }
 
-    public static int DupeTypes(int[] cardarray) {
+    public static int dupeTypes(int[] cardarray) {
         int duplicateColors = 0;
         for (int i = 0; i < cardarray.length; i++) {
             for (int j = 0; i < cardarray.length; i++) {
@@ -61,30 +61,58 @@ public class Poker {
         return duplicateColors;
     }
 
-    public static boolean checkForOnePair(int[] cards) {
-        return DupeValues(cards) == 2;
+    public static boolean paar(int[] cards) {
+        return dupeValues(cards) == 2;
     }
 
-    public static boolean checkForTwoPairs(int[] cards) {
-        return DupeValues(cards) == 4;
+    public static boolean zweipaare(int[] cards) {
+        return dupeValues(cards) == 4;
     }
 
-    public static boolean checkForThreeOfAKind(int[] cards) {
-        return DupeValues(cards) == 6;
+    public static boolean drillinge(int[] cards) {
+        return dupeValues(cards) == 6;
     }
-    public static boolean checkForStraight(int[] cards) {
+
+    public static boolean straight(int[] cards) {
         if (cards[0] == 0 && cards[1] == 1 && cards[2] == 2 && cards[3] == 3 && cards[4] == 12)
             return true;
         for (int i = 1; i < cards.length; i++) {
-            if (CardValue(cards[i]) - CardValue(cards[i - 1])) != 1) {
+            if ((cardValue(cards[i]) - cardValue(cards[i - 1])) != 1) {
                 return false;
             }
         }
         return true;
     }
 
-    public static boolean checkForFlush(int[] cards) {
-        return countDuplicateTypes(cards) == 4;
+    public static boolean flush(int[] cards) {
+        return dupeTypes(cards) == 4;
     }
 
+    public static boolean fullHouse(int[] cards) {
+        return dupeValues(cards) == 8;
+    }
+
+    public static boolean vierling(int[] cards) {
+        return dupeValues(cards) == 12;
+    }
+
+    public static boolean straightFlush(int[] cards) {
+        return straight(cards) && flush(cards);
+    }
+
+    public static boolean royalFlush(int[] cards) {
+        int storage = cardValue(cards[cards.length - 1]);
+        if (storage == 0) {
+            storage = cardValue(cards[0]);
+            for (int i = 1; i < cards.length - 1; i++) {
+                if (storage + 1 == cardValue(cards[i])) {
+                    royalflash = true;
+                    storage = cardValue(cards[i]);
+                } else {
+                    royalflash = false;
+                }
+            }
+        }
+        return royalflash;
+    }
 }
